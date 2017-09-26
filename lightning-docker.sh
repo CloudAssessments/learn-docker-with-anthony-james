33a91c70841ef0779523812cc44ff0779605c341
Your Name <you@example.com>
2017-09-26 16:21:03 +0000

    adding lightning docker script

---
+++ lightning-docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Script is used to install docker and build a quick application as a demo
+is_centos6()
+{
+    grep '^CentOS release 6.*$' /etc/centos-release >/dev/null 2>&1
+}
+is_centos7()
+{
+    grep '^ID=.centos.$' /etc/os-release >/dev/null 2>&1 && grep '^VERSION_ID=.7.*$' /etc/os-release >/dev/null 2>&1
+}
+is_rhel7()
+{
+    grep '^ID=.rhel.$' /etc/os-release >/dev/null 2>&1 && grep '^VERSION_ID=.7.*$' /etc/os-release >/dev/null 2>&1
+}
+is_ubuntu14()
+{
+    grep '^DISTRIB_ID=Ubuntu$' /etc/lsb-release >/dev/null 2>&1 && grep '^DISTRIB_CODENAME=trusty$' /etc/lsb-release >/dev/null 2>&1
+}
+is_ubuntu16()
+{
+    grep '^DISTRIB_ID=Ubuntu$' /etc/lsb-release >/dev/null 2>&1 && grep '^DISTRIB_CODENAME=xenial$' /etc/lsb-release >/dev/null 2>&1
+}
+if is_centos6 || is_ubuntu14 || is_ubuntu16
+then
+        echo -e "This course is designed for CentOS7 or Red Hat 7, Please use one of those distributions"
+elif is_centos7 || is_rhel7
+then
+        /usr/bin/yum clean all
+        /usr/bin/yum yum install -y yum-utils device-mapper-persistent-data lvm2
+        /usr/bin/yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        /usr/bin/yum install docker-ce
+        /bin/systemctl start docker
+        /usr/bin/yum install -y git
+        /bin/git clone https://github.com/CloudAssessments/fs-photos.git
+        cd fs-photos
+        echo -e "\n" >> Dockerfile
+        echo -e  CMD [\"npm\", \"run\", \"prod\"] >> Dockerfile
+       /bin/docker build .
+fi
